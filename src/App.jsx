81f71bc29ad958b5d4ef6c37bd58c6c06970be10
siteKatalog <sitekatalogg@gmail.com>
2023-03-25 16:58:33 +0300

    small optimizations

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   useEffect(() => {
+    //check if hidden elements is intersecting
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -20,17 +21,24 @@ function App() {
     const hiddenElements = document.querySelectorAll(".hidden");
     hiddenElements.forEach((el) => observer.observe(el));
 
+    //check if navbar scrolled
     const mainSection = document.querySelector(".mainSection");
     const navbar = document.querySelector(".navbar");
     const navbarWave = document.querySelector(".navbarWave");
     mainSection.addEventListener("scroll", () => {
-      if (mainSection.scrollTop >= 80) {
+      if (mainSection.scrollTop >= 40) {
         navbar.classList.add("scrolled");
         navbarWave.classList.add("scrolled");
       } else {
         navbar.classList.remove("scrolled");
         navbarWave.classList.remove("scrolled");
       }
+    });
+
+    //set img loadings to lazy
+    const imgs = document.querySelectorAll("img");
+    imgs.forEach((el) => {
+      el.setAttribute("loading", "lazys");
     });
   }, []);
   return (
